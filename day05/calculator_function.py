a0298a11f84a3b090a0f351ec9feb5e3048b12f4
Ali Talebi <alitalebi7562@gmail.com>
2026-07-18 12:46:13 +0330

    Add calculator using functions

---
+++ day05/calculator_function.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+x = int(input("First number: "))
+y = int(input("Second number: "))
+
+print("Sum:", add(x, y))
+print("Difference:", subtract(x, y))
