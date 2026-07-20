2e9fa68a334dfe1b6e0cf402f61de5ba3eac26d9
Ali Talebi <alitalebi7562@gmail.com>
2026-07-20 12:15:19 +0330

    Add student information program

---
+++ day07/student_info.py
@@ -0,0 +1,8 @@
+student = {
+    "name": input("Enter your name: "),
+    "age": int(input("Enter your age: ")),
+    "city": input("Enter your city: ")
+}
+
+print("\nStudent Information")
+print(student)
